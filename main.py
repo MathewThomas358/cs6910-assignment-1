aa618b7b994ffe8e03333c57424b53b20d92d4f1
Mathew Thomas <mathewthomas358@gmail.com>
2023-02-16 23:17:48 +0530

    Added partial implementation of Q1. Squashed two commits.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ for Classification
 '''
 
 from keras.datasets import fashion_mnist #as data
+from PIL import Image
 
 import numpy as np
 import pandas as pd
@@ -16,12 +17,15 @@ wb.init(project = "cs6910-assignment-1")
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-
+images = []
 for i in range(10):
 
     index = np.where(y_train == i)
     # print()
+    images.append(x_train[index[0][0]])
+    # wb.log({str(i) : [wb.Image(x_train[index[0][0]])]})
 
-    wb.log({str(i) : [wb.Image(x_train[index[0][0]])]})
-
+images = [Image.fromarray(img) for img in images]
+images_as_np = np.asarray(images)
+wb.log({"Images": wb.Image(images_as_np)})
 wb.finish()
