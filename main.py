b86e9b240dd1d3045afd723380aebbc50d9b9513
Mathew Thomas <mathewthomas358@gmail.com>
2023-02-26 14:40:53 +0530

    Completed implementation of Q1

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 '''
 CS6910 - Assignment 1
-Implementation of Gradient Descent with Backpropagation
-for Classification
+Implementation of Feedforward Neural Network with Backpropagation
 
 @author: cs22m056
 '''
 
-from keras.datasets import fashion_mnist #as data
-from PIL import Image
+from keras.datasets import fashion_mnist
 
 import numpy as np
-import pandas as pd
 import wandb as wb
 
-wb.init(project = "cs6910-assignment-1")
+wb.init(project="cs6910-assignment-1")
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-images = []
-for i in range(10):
 
-    index = np.where(y_train == i)
-    # print()
-    images.append(x_train[index[0][0]])
-    # wb.log({str(i) : [wb.Image(x_train[index[0][0]])]})
+def plot_images():
+    """ Function to plot sample images from each class """
 
-images = [Image.fromarray(img) for img in images]
-images_as_np = np.asarray(images)
-wb.log({"Images": wb.Image(images_as_np)})
-wb.finish()
+    images = []
+    labels = []
+    for i in range(10):
+
+        index = np.where(y_train == i)
+        images.append(x_train[index[0][0]])
+        labels.append(class_names[i])
+
+    wb.log({"Samples from each class": [wb.Image(
+        img, caption=caption) for img, caption in zip(images, labels)]})
+
+
+if __name__ == "__main__":
+
+    plot_images()
+    wb.finish()
